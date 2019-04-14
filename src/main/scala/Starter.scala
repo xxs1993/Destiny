4c507409fdb8feda4eec5a48203f946b9c0588bf
xxs1993 <xiao.x@husky.neu.edu>
2019-04-13 20:51:11 -0400

    update

--- src/main/scala/Starter.scala
+++ src/main/scala/Starter.scala
@@ -3,11 +3,11 @@ import akka.util.Timeout
 
 object Starter extends App{
   override def main(args:Array[String])={
-//    Crawler.crawl(1)
-//implicit val timeout: Timeout = 10 second
-    implicit val system = ActorSystem("MatchActor")
-    val actor = system.actorOf(Props.create(classOf[MatchActor]), "account")
-    actor ! "account/2019_4_13_account"
+    Crawler.crawl(1)
+////implicit val timeout: Timeout = 10 second
+//    implicit val system = ActorSystem("MatchActor")
+//    val actor = system.actorOf(Props.create(classOf[MatchActor]), "account")
+//    actor ! "account/2019_4_13_account"
   }
 
 }
