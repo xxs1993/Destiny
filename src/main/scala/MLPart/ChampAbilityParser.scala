8e67120d5d858e270bf89f5c559b2284e4d9e6b1
leiliunu <liu.lei1@husky.neu.edu>
2019-04-21 19:20:51 -0400

    method: win rate prediction on winning rate

--- src/main/scala/MLPart/ChampAbilityParser.scala
+++ src/main/scala/MLPart/ChampAbilityParser.scala
@@ -18,23 +18,11 @@ object ChampAbilityParser extends App{
       .appName("ChampAbilityParser")
       .getOrCreate()
 
-    import spark.implicits._
 
-    val raw = spark.read.format("csv").option("header", "true").load("data/team_match.csv")
-    var champ_position_map: Map[String, Set[Int]] = Map()
-    champ_position_map += ("top" -> (raw.selectExpr("cast(Win_TOP as Int) Win_TOP").map(r => r(0).asInstanceOf[Int]).collect()
-      ++: raw.selectExpr("cast(Fail_TOP as Int) top").map(_(0).asInstanceOf[Int]).collect()).toSet)
-    champ_position_map += ("jg" -> (raw.selectExpr("cast(Win_JUG as Int) jg").map(r => r(0).asInstanceOf[Int]).collect()
-      ++: raw.selectExpr("cast(Fail_JUG as Int) jg").map(_(0).asInstanceOf[Int]).collect()).toSet)
-    champ_position_map += ("mid" -> (raw.selectExpr("cast(Win_MID as Int) mid").map(r => r(0).asInstanceOf[Int]).collect()
-      ++: raw.selectExpr("cast(Fail_MID as Int) mid").map(_(0).asInstanceOf[Int]).collect()).toSet)
-    champ_position_map += ("bot" -> (raw.selectExpr("cast(Win_BOT as Int) bot").map(r => r(0).asInstanceOf[Int]).collect()
-      ++: raw.selectExpr("cast(Fail_BOT as Int) bot").map(_(0).asInstanceOf[Int]).collect()).toSet)
-    champ_position_map += ("sup" -> (raw.selectExpr("cast(Win_SUP as Int) sup").map(r => r(0).asInstanceOf[Int]).collect()
-      ++: raw.selectExpr("cast(Fail_SUP as Int) sup").map(_(0).asInstanceOf[Int]).collect()).toSet)
+    val rate = WinRate().predictWinRate(List(92,120,55,22,555,266,104,99,222,40))
 
-    champ_position_map.foreach(println(_))
-    println(champ_position_map("sup").contains(203))
+    println(rate)
+
 
     spark.stop()
 
